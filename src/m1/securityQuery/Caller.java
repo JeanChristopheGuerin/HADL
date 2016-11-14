c6d0e504ed758f5b1c2fe521dc3c10d9b604c1ad
JC <jcgmoaa@gmail.com>
2016-11-14 06:25:52 -0800

    Gros ajout

---
+++ src/m1/securityQuery/Caller.java
@@ -0,0 +1,11 @@
+package m1.securityQuery;
+import m2.connecteur.RoleTo;
+
+public class Caller extends RoleTo {
+
+	public Caller() {
+		super("Caller");
+		// TODO Auto-generated constructor stub
+	}
+
+}
